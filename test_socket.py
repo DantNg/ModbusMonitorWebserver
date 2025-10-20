23e226d079404e023f88886fdc02688f2d141990
DantNg <nguyendatmc581@gmail.com>
2025-10-20 10:45:12 +0700

    update UI alarm

--- test_socket.py
+++ test_socket.py
@@ -9,16 +9,13 @@ def send_alarm_event():
     # sio.emit('join_subdashboard', {'room': 'subdashboard_19'})
     time.sleep(0.2)  # Đợi server xác nhận join (nên có)
     data = {
-        'id': 999,
-        'title': 'Test Alarm',
-        'message': 'Test alarm from Python client',
-        'level': 'Critical',
-        'tag_id': 123,
-        'tag_name': 'TestTag',
-        'device_name': 'TestDevice',
-        'value': 42.0,
-        'status': 'Active',
-        'created_at': time.strftime('%Y-%m-%dT%H:%M:%S')
+        "title": f"Alarm Triggered: hello",
+        "message": f"Alarm cleared: ",
+        "level": "High",
+        "tag_name": 123,
+        "value": 123,
+        "status": "INCOMING",
+        "created_at": time.strftime('%Y-%m-%dT%H:%M:%S')
     }
     sio.emit('alarm_event', data)
     print('Sent alarm_event:', data)
