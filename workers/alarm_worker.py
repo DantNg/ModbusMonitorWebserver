5cd91041f84ea99dc6f2c24e0667b810b966b3ed
DantNg <nguyendatmc581@gmail.com>
2025-10-21 23:47:32 +0700

    update alarm

--- workers/alarm_worker.py
+++ workers/alarm_worker.py
@@ -48,7 +48,22 @@ sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'webapp
 
 logger = logging.getLogger(__name__)
 sio = socketio.Client()
-sio.connect('http://localhost:5000')
+def get_local_ip():
+    """Get the local IP address of the current machine."""
+    import socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Doesn't have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+server_ip = get_local_ip()
+sio.connect(f'http://{server_ip}:5000')
 @dataclass
 class AlarmConfig:
     """Configuration for alarm worker"""
